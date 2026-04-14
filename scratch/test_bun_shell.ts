fba23b85910deb763f283462fb61e9dfd2f5342c
Tekizaki <dzakitaufiqullah@gmail.com>
2026-04-14 07:25:21 +0700

    feat: remove persistent memory system and tools

--- scratch/test_bun_shell.ts
+++
@@ -1,6 +0,0 @@
-import { $ } from "bun";
-const query = "test query";
-const cmd = $`echo "${query}"`.toString();
-console.log("With quotes:", cmd);
-const cmd2 = $`echo ${query}`.toString();
-console.log("Without quotes:", cmd2);
